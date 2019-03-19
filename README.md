f541e31c8edd16c5571b7b00c3f0a50af99fb3dd
Doc <doctorwhofan967@gmail.com>
2019-03-18 19:07:21 -0500

    First commit
    All the code I've done so far to be deployed on heroku.

---
+++ README.md
@@ -0,0 +1,2 @@
+# PewDieB0t
+A bot that tweets out the subcount of pewds and tseries and the subcount every hour and alerts when it it too low or in the negative.
---
+++ bot.js
@@ -0,0 +1,84 @@
+console.log('Bot is starting!');
+const Twit = require('twit');
+const Config = require('./config');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+const $ = jQuery = require('jquery')(window);
+
+const KEY = process.env.YT_KEY;
+var T = new Twit(Config);
+
+const dangerZone = 20000;
+
+tweetIt(`The subgap is currently ${subGap()}! #PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+
+tweetSubCount();
+alertGap();
+
+/**
+  * Tweets something out.
+  * @param {string} statusText - What you're tweeting.
+  */
+function tweetIt(statusText)
+{
+    T.post('statuses/update', { status: statusText }, function(err, data, response) {
+        if(err) console.log("Something went horribly wrong!")
+        else console.log(`Tweeted: '${statusText}'`);
+    });
+}
+
+/**
+  * Tweets something out.
+  * @param {string} channel - The channel you are trying to get the sub count of.
+  */
+function getChannelData(channel)
+{
+    var ytApiUrl = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=' + channel + '&key=' + KEY;
+    var subsVar;
+    
+    $.ajaxSetup({
+        async: false
+    });
+
+    $.getJSON(ytApiUrl, function(result){
+       subsVar = +result['items'][0]['statistics'].subscriberCount;
+    });    
+    
+    return subsVar;
+}
+
+function subGap()
+{
+    let pewdsSubCount = getChannelData('pewdiepie');
+    let tseriesSubCount = getChannelData('tseries');
+    return pewdsSubCount - tseriesSubCount;
+}
+
+async function alertGap()
+{
+    setInterval(function(){
+        let subgap = subGap();
+        if(subgap < dangerZone)
+        {
+            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 THE SUBGAP IS NOW ${subgap}! WE NEED TO DO SOMETHING! THIS CANNOT BE THE END! #PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+        }
+        else if(subgap < 0)
+        {
+            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 THE SUBGAP IS NOW NEGATIVE! ${subgap}! EVERYONE SUBSCRIBE TO GET PEWDIEPIE BACK! THIS IS NOT THE END! LETS GET #Sub2Pewds TRENDING! #PewDiePie #TSeries #MemeReview #Subgap`);
+        }
+    }, 1000*60*15);
+}
+
+async function tweetSubCount()
+{
+    setInterval(function(){
+        let subgap = subGap();
+        let pewdsCount = getChannelData('pewdiepie');
+        let tCount = getChannelData('tseries');
+
+        tweetIt(`PewDiePie currently has ${pewdsCount}. T-Series currently has ${tCount}. The subgap is currenly at ${subgap}. #PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+    }, 1000*60*60)
+}
