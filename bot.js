f604816e4e36578ef7c3f2961c3093a7dfc326ee
Doc <doctorwhofan967@gmail.com>
2019-03-20 14:21:50 -0500

    Less annoying
    Made the bot less annoying, added liking tweets that it replies to and its own tweets, and made the message at the end of tweets randomize.

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 console.log('Bot is starting!');
+
 const Twit = require('twit');
 const Config = require('./config');
 const jsdom = require("jsdom");
@@ -8,18 +9,28 @@ const { document } = (new JSDOM('')).window;
 global.document = document;
 const $ = jQuery = require('jquery')(window);
 
+const T = new Twit(Config);
 const KEY = process.env.YT_KEY;
-var T = new Twit(Config);
-
 const dangerZone = 10000;
 const maxTweetsInHour = 10;
+const messages = [
+    'We will not stop fighting!', 
+    'This is not the end!', 
+    'Everyone subscribe!',
+    'This cannot be the end!',
+    'We need to do something!',
+    'A sub audit could screw us over!'
+];
+
 var tweetsInHour = 0;
 var replyToId;
 var previousReplyId;
-var previousGap;
+var previousGap = 1;
 
-tweetIt(`Bot has started. \n\nThe subgap is currently ${subGap()}! \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+console.log(`Bot has started!`)
+tweetIt(`The subgap is currently ${subGap()}! \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
 
+//All async functions called
 alertGap();
 negativeAlert();
 tweetSubCount();
@@ -37,40 +48,40 @@ async function alertGap()
 {
     setInterval(function(){
         let subgap = subGap();
-        if(subgap < 5000)
+        if(subgap < 500 && subgap > 0)
         {
-            tweetIt(`🚨🚨ALERT! SUBGAP < 5,000 ALERT!🚨🚨 \n\nTHE SUBGAP IS NOW ${subgap.toLocaleString('en')}! \nWE NEED TO DO SOMETHING! THIS CANNOT BE THE END! \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+            tweetIt(`🚨🚨ALERT! SUBGAP < 500 ALERT!🚨🚨 \n\nTHE SUBGAP IS NOW ${subgap.toLocaleString('en')}! \n${selectMessage().toUpperCase()} \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
         }
-        else if(subgap < 1000)
+        else if(subgap < 1000 && subgap > 0)
         {
-            tweetIt(`🚨🚨ALERT! SUBGAP < 1,000 ALERT!🚨🚨 \n\nTHE SUBGAP IS NOW ${subgap.toLocaleString('en')}! \nWE NEED TO DO SOMETHING! THIS CANNOT BE THE END! \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+            tweetIt(`🚨🚨ALERT! SUBGAP < 1,000 ALERT!🚨🚨 \n\nThe subgap is now ${subgap.toLocaleString('en')}! \n${selectMessage()} \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
         }
-        else if(subgap < 500)
+        else if(subgap < 5000 && subgap > 0)
         {
-            tweetIt(`🚨🚨ALERT! SUBGAP < 500 ALERT!🚨🚨 \n\nTHE SUBGAP IS NOW ${subgap.toLocaleString('en')}! \nWE NEED TO DO SOMETHING! THIS CANNOT BE THE END! \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
-        }
-        else if(subgap < dangerZone)
+            tweetIt(`🚨🚨ALERT! SUBGAP < 5,000 ALERT!🚨🚨 \n\nThe subgap is now ${subgap.toLocaleString('en')}! \n${selectMessage()} \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+        }        
+        else if(subgap < dangerZone && subgap > 0)
         {
-            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 \n\nTHE SUBGAP IS NOW ${subgap.toLocaleString('en')}! \nWE NEED TO DO SOMETHING! THIS CANNOT BE THE END! \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
+            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 \n\nThe subgap is now ${subgap.toLocaleString('en')}! \n${selectMessage()} \n\n#PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
         }
-    }, 1000*60*30);
+    }, 1000*60*60);
 }
 
 async function negativeAlert()
 {
     setInterval(function(){
         let subgap = subGap();
-        if(subgap < 0)
+        if(subgap < 0 && previousGap > 0)
         {
-            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 THE SUBGAP IS NOW NEGATIVE! \n\nT-SERIES HAS PASSED PEWDS! ${subgap.toLocaleString('en')}! \nEVERYONE SUBSCRIBE TO GET PEWDIEPIE BACK! THIS IS NOT THE END! LETS GET \n\n#Sub2Pewds TRENDING! #PewDiePie #TSeries #MemeReview #Subgap`);
+            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 THE SUBGAP IS NOW NEGATIVE! ${subgap.toLocaleString('en')}! \n\nT-SERIES HAS PASSED PEWDS! THIS IS NOT THE END! \nEVERYONE SUBSCRIBE TO GET PEWDIEPIE BACK! ${selectMessage().toUpperCase()} LETS GET #Sub2Pewds TRENDING! \n\n#PewDiePie #TSeries #MemeReview #Subgap`);
             previousGap = subgap;
         }
         else if(subgap > 0 && previousGap < 0)
         {
-            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 THE GAP IS POSITIVE AGAIN! \n\n${subgap.toLocaleString('en')}! \nWE WILL NOT STOP FIGHTING! \n\n#PewDiePie #TSeries #MemeReview #Subgap`);
+            tweetIt(`🚨🚨ALERT! ALERT!🚨🚨 THE GAP IS POSITIVE AGAIN! \n\n${subgap.toLocaleString('en')}! \nWE WILL KEEP FIGHTING! \n\n#PewDiePie #TSeries #MemeReview #Subgap`);
             previousGap = subgap;
         }
-    }, 1000*60*2)
+    }, 1000*60*1)
 }
 
 async function tweetSubCount()
@@ -81,7 +92,7 @@ async function tweetSubCount()
         let tCount = getChannelData('tseries').toLocaleString('en');
 
         tweetIt(`PewDiePie currently has ${pewdsCount}. T-Series currently has ${tCount}. The subgap is currenly at ${subgap}. #PewDiePie #TSeries #MemeReview #Subgap #Sub2Pewds`);
-    }, 1000*60*60*5)
+    }, 1000*60*60*6)
 }
 
 async function resetConstraints()
@@ -89,6 +100,13 @@ async function resetConstraints()
     setInterval(function(){
         tweetsInHour = 0;
     }, 1000*60*60)
+}
+
+function selectMessage()
+{
+    let r = Math.floor(Math.random()*messages.length);
+
+    return messages[r];
 }
 
 /**
@@ -103,20 +121,31 @@ async function reply(screenName, tweet)
             replyToId = data[0].id_str;
 
             var opts = {
-                in_reply_to_status_id: replyToId
-              , status: `@${screenName} ${tweet}`
+                in_reply_to_status_id: replyToId,
+                status: `@${screenName} ${tweet}`
             }
 
             if(replyToId !== null && replyToId !== previousReplyId)
             {
                 T.post('statuses/update', opts, function(err, data, response){
-                        if(err) console.log(`Something went horribly wrong!: ${err}`)
-                        else { console.log(`Tweeted: '${tweet}' in response to: ${screenName}`); previousReplyId = replyToId; replyToId = null; };
-                    }
-                )
+                    if(err) console.log(`Something went horribly wrong!: ${err}`)
+                    else { console.log(`Tweeted: '${tweet}' in response to: ${screenName}`); previousReplyId = replyToId; replyToId = null; likeTweet(replyToId); likeTweet(data.id_str); };
+                });
             }
         });
     }, 1000*15)
+}
+
+/**
+  * Deletes a tweet.
+  * @param {string} id_str - The tweet you're deleting.
+  */
+function deleteTweet(id_str)
+{
+    T.post('statuses/destroy/:id', { id: id_str }, function(err, data, response){
+        if(err) console.log(`Something went horribly wrong!: ${err}`)
+        else {  }
+    });
 }
 
 /**
@@ -134,13 +163,25 @@ function tweetIt(statusText)
     {
         T.post('statuses/update', { status: statusText }, function(err, data, response) {
             if(err) console.log(`Something went horribly wrong!: ${err}`)
-            else { console.log(`Tweeted: '${statusText}'`); tweetsInHour++; }
+            else { console.log(`Tweeted: '${statusText}'`); tweetsInHour++; likeTweet(data.id_str); }
         });
     }
 }
+
+/**
+  * Likes a tweet.
+  * @param {string} id_str - The string id of the tweet you are liking.
+  */
+function likeTweet(id_str)
+{
+    T.post('favorites/create', { id: id_str }, function(err, data, response){
+        if(err) console.log(`Something went horribly wrong!: ${err}`)
+        else { console.log(`Liked a tweet with id: ${id_str}`); };
+    });
+}
  
 /**
-  * Tweets something out.
+  * Gets the sub count of a channel.
   * @param {string} channel - The channel you are trying to get the sub count of.
   */
 function getChannelData(channel)
